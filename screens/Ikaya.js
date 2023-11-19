f94ef40a2de16d892371286958ed7dd19a01e656
Marinkie Thupi <marinkiethupi@ip-192-168-1-12.af-south-1.compute.internal>
2023-11-20 00:32:57 +0200

    added comments to Ikaya screen

--- screens/Ikaya.js
+++ screens/Ikaya.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Ikaya = () => {
   return (
 // TODO: Indicate any pending tasks or future improvements.
-// Need to add content to this screen?
+// Need to add content to this screen as the home page?
     <View>
       <Text>Ikaya</Text>
     </View>
