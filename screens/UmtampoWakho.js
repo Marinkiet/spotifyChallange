ef275583904d4328811c5e67d1722f18876669cb
Marinkie Thupi <marinkiethupi@ip-192-168-1-12.af-south-1.compute.internal>
2023-11-19 23:28:02 +0200

    added ikhaya and umtapo wako screens to screens folder

---
+++ screens/UmtampoWakho.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const UmtampoWakho = () => {
+  return (
+    <View>
+      <Text>Umtampo-Wakho</Text>
+    </View>
+  )
+}
+
+export default UmtampoWakho
