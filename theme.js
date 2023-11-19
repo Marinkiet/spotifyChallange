5a3fd56c781cdea3341ab810ed3d571a67413161
Marinkie Thupi <marinkiethupi@ip-192-168-1-12.af-south-1.compute.internal>
2023-11-19 23:28:57 +0200

    added theme folder

---
+++ theme.js
@@ -0,0 +1,20 @@
+//Circular fonts from spotify
+export const themeFonts = {
+    circularLight:'CircularSpotifyText-Light',
+    circularLightItalic:'CircularSpotifyText-LightItalic',
+    circularBook:'CircularStd-Book',
+    circularBookItalic:'CircularStd-BookItalic',
+    circularMeduim:'CircularStd-Medium',
+    circularMeduimItalic:'CircularStd-MediumItalic',
+    circularBold:'CircularStd-Bold',
+    circularBoldItalic:'CircularStd-BoldItalic',
+    circularBlack:'CircularStd-Black',
+    circularBlackItalic:'CircularStd-BlackItalic',
+}
+export const themeTextColor = {
+    secondary: (opacity) => `rgba(176,176,176,${opacity})`,
+    primary: (opacity) => `rgba(249,249,249,${opacity})`,
+    bgColor: (opacity) => `rgba(40,40,40,${opacity})`,
+    IconGreen:(opacity)=>`rgba(28,185,86,${opacity})`,
+    IconGrey:(opacity)=>`rgba(0,0,0,${opacity})`,
+}
